166aa52ee41e0cec3ca6cded50aea3c8cd48a19c
Alexander Rodionov <tandav@tandav.me>
2022-07-19 21:23:38 +0300

    WIP mvp works

--- chrome_extension/popup.js
+++ chrome_extension/popup.js
@@ -3,16 +3,34 @@
 //     alert("hello");
 // }
 
+
+
 const save_tabs = () => {
-    // const tabs = []
-    chrome.tabs.query({}, function(tabs) {
+    chrome.tabs.query({}, function (tabs) {
+        const tabs_out = []
         tabs.forEach(function (tab) {
-            // console.log(tab);
-            console.log(tab.windowId, tab.url, tab.title, tab.favIconUrl);
-        });
-    });
+            // console.log(tab)
+            // console.log(tab.windowId, tab.url, tab.title, tab.favIconUrl)
+            // console.log({url: tab.url, title: tab.title, favIconUrl: tab.favIconUrl, windowId: tab.windowId})
+            tabs_out.push({url: tab.url, title: tab.title, favIconUrl: tab.favIconUrl, windowId: tab.windowId})
+        })
+        const opts = {
+            method: 'POST',
+            headers: {'content-type': 'application/json'},
+            body: JSON.stringify(tabs_out)
+        }
+        const endpoint = 'http://localhost:5006/'
+        fetch(endpoint, opts).then(response => response.json()).then(data => {
+            console.log(data)
+        })
+    })
+    // .then(tabs_out => {
+    // console.log(tabs_out)
+    //
+    //     fetch(endpoint, opts).then(response => console.log(response.json()))
+    // })
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById("save_tabs_button").addEventListener("click", save_tabs);
-});
+    document.getElementById("save_tabs_button").addEventListener("click", save_tabs)
+})
--- server.py
+++ server.py
@@ -1,24 +1,57 @@
-import pathlib
-# FLASK
+# import pathlib
+# # FLASK
+#
+# from flask import Flask, request, jsonify, Response, abort, send_file
+#
+# app = Flask(__name__)
+# app.config['JSON_AS_ASCII'] = False
+#
+#
+#
+# @app.after_request
+# def after_request(response):
+#     response.headers.add('Access-Control-Allow-Origin', '*')
+#     response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+#     response.headers.add('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE')
+#     return response
+#
+#
+# @app.route('/bookmark', methods=['POST'])
+# def file_info():
+#     print(request.get_json())
+#     # path = request.get_json()['path']
+#     # return jsonify(path_utils.file_info(path))
+#     return Response(None, 200)
 
-from flask import Flask, request, jsonify, Response, abort, send_file
 
-app = Flask(__name__)
-app.config['JSON_AS_ASCII'] = False
+from fastapi import Request
 
-
-
-@app.after_request
-def after_request(response):
-    response.headers.add('Access-Control-Allow-Origin', '*')
-    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
-    response.headers.add('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE')
-    return response
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 
-@app.route('/bookmark', methods=['POST'])
-def file_info():
-    print(request.get_json())
-    # path = request.get_json()['path']
-    # return jsonify(path_utils.file_info(path))
-    return Response(None, 200)
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    # allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.post('/')
+async def save_tabs(request: Request):
+    payload = await request.json()
+    print(payload)
+    return {'status': 'ok'}
+
+# @app.post('/')
+# async def save_tabs(request: Request):
+#     if not secrets.compare_digest(token, TOKEN):
+#         raise HTTPException(status.HTTP_401_UNAUTHORIZED, detail='access denied: invalid token')
+#
+#     code = subprocess.run(COMMAND, check=False, cwd=CWD).returncode
+#     if code == 0:
+#         return {'status': 'success'}
+#     raise HTTPException(status.HTTP_500_INTERNAL_SERVER_ERROR, f'command failed with exit code: {code}')
