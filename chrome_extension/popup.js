015f169a69625aa260069941b64ee549d49ddf72
Alexander Rodionov <tandav@tandav.me>
2022-07-19 20:54:28 +0300

    WIP backup

--- chrome_extension/popup.js
+++ chrome_extension/popup.js
@@ -1,5 +1,18 @@
 
+// const handler = () => {
+//     alert("hello");
+// }
 
-const do_alert = () => {
-    alert("pizda");
+const save_tabs = () => {
+    // const tabs = []
+    chrome.tabs.query({}, function(tabs) {
+        tabs.forEach(function (tab) {
+            // console.log(tab);
+            console.log(tab.windowId, tab.url, tab.title, tab.favIconUrl);
+        });
+    });
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById("save_tabs_button").addEventListener("click", save_tabs);
+});
